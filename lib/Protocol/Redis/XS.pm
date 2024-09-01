9f264e4bb4816f45bd4bd265905992f17a08b075
Dan Book <grinnz@grinnz.com>
2024-09-01 02:27:42 -0400

    avoid context sensitive return on invalid api value

--- lib/Protocol/Redis/XS.pm
+++ lib/Protocol/Redis/XS.pm
@@ -16,7 +16,7 @@ sub new {
   my $on_message = delete $args{on_message};
 
   my $self = bless \%args, $class;
-  return unless $self->api == 1;
+  return undef unless $self->api == 1;
   $self->on_message($on_message) if defined $on_message;
 
   $self->_create;
